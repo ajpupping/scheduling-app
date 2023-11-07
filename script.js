4ce2c9de83044bd03950aa1fbea70a8de87a50e7
Alyssa <125580811+ajpupping@users.noreply.github.com>
2023-11-06 21:21:01 -0800

    edited time blocks, added event listener

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 var displayDateEl = $('#currentDay');
+var saveButton = $('.saveBtn');
 
 function displayDate() {
     var currentDate = dayjs().format('dddd, MMMM D, YYYY');
@@ -18,6 +19,13 @@ $(function () {
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
     //
+    $('.saveBtn').each(function() {
+        $(this).on("click", function() {
+            let textId = $(this).siblings("div").attr("id");
+            let scheduleItem = $(this).siblings('textarea').val();
+            localStorage.setItem(textId, JSON.stringify(scheduleItem));
+        });
+    });
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
