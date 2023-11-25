3f1ac7ac8e2d8f1da4d10d2e396daceaad987072
Alyssa <125580811+ajpupping@users.noreply.github.com>
2023-11-24 20:49:07 -0800

    improved save function to store both time and text

--- script.js
+++ script.js
@@ -23,9 +23,12 @@ $(function () {
     //
     $('.saveBtn').each(function() {
         $(this).on("click", function() {
-            let textId = $(this).siblings("div").attr("id");
-            let scheduleItem = $(this).siblings('textarea').val();
-            localStorage.setItem(textId, JSON.stringify(scheduleItem));
+            var taskText = $(this).siblings('.description').val();
+            console.log('text: ' + taskText)
+            var taskTime = $(this).parent().attr('id').substring(5);
+            console.log('time: ' + taskTime)
+
+            localStorage.setItem(taskText, taskTime);
         });
     });
 
@@ -53,16 +56,6 @@ $(function () {
     }
 
     changeColor()
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-
-
-
-
 
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
