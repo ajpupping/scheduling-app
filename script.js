810d85196802533a9717f851904ab5483be5408a
Alyssa <125580811+ajpupping@users.noreply.github.com>
2023-11-24 20:31:45 -0800

    added code to change the color of blocks based on current time

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
 var displayDateEl = $('#currentDay');
 var saveButton = $('.saveBtn');
+
 
 function displayDate() {
     var currentDate = dayjs().format('dddd, MMMM D, YYYY');
     displayDateEl.text(currentDate);
 }
 
-displayDate()
+displayDate();
 
 $(function () {
     // TODO: Add a listener for click events on the save button. This code should
@@ -26,12 +28,42 @@ $(function () {
             localStorage.setItem(textId, JSON.stringify(scheduleItem));
         });
     });
+
+    function changeColor() {
+        var currentHour = dayjs().hour();
+        console.log(currentHour);
+        
+        $(".time-block").each(function () {
+            var presentBlock = parseInt($(this).attr("id").substring(5));
+            console.log(presentBlock)
+            if(presentBlock === currentHour) {
+                $(this).addClass("present");
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+            } else if(presentBlock < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("present");
+                $(this).removeClass("future");
+            } else {
+                $(this).addClass("future");
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+            }
+        });    
+    }
+
+    changeColor()
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
     //
+
+
+
+
+
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
